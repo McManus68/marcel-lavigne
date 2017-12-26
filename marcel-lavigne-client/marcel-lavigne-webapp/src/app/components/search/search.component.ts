c1ee59723f74c60c75b81d05b22a8241f7b02032
Manu <emmanuel.tarrou34@gmail.com>
2017-12-27 00:47:40 +0100

    ff

--- marcel-lavigne-client/marcel-lavigne-webapp/src/app/components/search/search.component.ts
+++ marcel-lavigne-client/marcel-lavigne-webapp/src/app/components/search/search.component.ts
@@ -1,9 +1,25 @@
 import { Component, Input } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'ml-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
+  animations: [
+      // Each unique animation requires its own trigger. The first argument of the trigger function is the name
+      trigger('rotatedState', [
+                            state('default', style({ transform: 'rotate(0)' })),
+                            state('rotated', style({ transform: 'rotate(-180deg)' })),
+                            transition('rotated => default', animate('300ms ease-out')),
+                            transition('default => rotated', animate('300ms ease-in'))
+     ])
+   ]
 })
 export class SearchComponent {
+
+  state: string = 'default';
+
+  rotate() {
+    this.state = (this.state === 'default' ? 'rotated' : 'default');
+  }
 }
